b86942ed8cd9f6886d9d8f70191cffb6763c34ee
Francesco De Toni <32432020+clancyoftheoverflow@users.noreply.github.com>
2022-02-21 22:14:46 +0800

    Update NER.py

--- NER/NER.py
+++ NER/NER.py
@@ -6,14 +6,13 @@ This script runs inference of T0++ on multiple GPUs using model parallelism.
 """
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import datasets
 import torch
 import pandas
 import re
 
-"TO DO: create function(prompt) for inference and call when needed rather than repeating code"
-
 # List of entity types
-ent_types = ["names of person", "names of location", "names of organization", "dates", "names of media", "names of doctrine"] # I split products into media and doctrines
+ent_types = ["PERS", "LOC", "ORG", "TIME", "PROD.MED", "PROD.DOC"] # I split products into media and doctrines as "products" is too general as a prompt
 
 # Upload model
 model_name = "bigscience/T0pp"
@@ -25,35 +24,37 @@ print("Model and tokenizer loaded")
 model.parallelize()
 print("Moved model to GPUs")
 
-"TO DO: cycle on languages"
-
-"TO DO: cycle on sentences"
-# Import sentence
-# Clean sentence
-# Regualrise spelling
-sentence = sentence
-
-"TO DO: cycle on prompts"
-df = pd.DataFrame(columns=["token", "entity_type"])
-
-for entity in ent_types:
-    prompt = "Input: {}.format(sentence)" + "\n" + "In input, what are the {}? Separate answers with commas".format(ent_types)
-
-    inputs = tokenizer.encode(prompt_YN, return_tensors="pt")
+# Inference function
+def T0_infer(prompt):
+    inputs = tokenizer.encode(prompt, return_tensors="pt")
     inputs = inputs.to("cuda:0")
     with torch.no_grad():
         outputs = model.generate(inputs)
 
     answer = (tokenizer.decode(outputs[0], skip_special_tokens=True))
 
+"TO DO: upload datset"
+
+"TO DO: cycle on sentences"
+# Import sentence
+# Clean sentence: remove at least split words (like "in", "-", "dustrious")
+# Regularise spelling
+sentence = sentence
+
+# Cycle over prompts
+df = pd.DataFrame(columns=["token", "entity_type"])
+ent_types_query = ["names of person", "names of location", "names of organization", "dates", "names of media", "names of doctrine"]
+for k in ent_types:
+    prompt = "Input: {}.format(sentence)" + "\n" + "In input, what are the {}? Separate answers with commas".format(ent_types_query)
+    answer = T0_infer(prompt)
     inferred_ents = answer.split(",")
 
     # Check if token exists and add it to dataframe    
     for token in inferred_ents:
         if re.search(token, sentence, re.IGNORECASE):
-            df = df.append({"token": token, "entity_type": entity}, ignore_index=True)
+            df = df.append({"token": token, "entity_type": ent_types[k]}, ignore_index=True)
 
-# Disambiguate 1: choose longest toekn when token is substring of another token
+# Disambiguate 1: choose longest token when token is substring of another token
 lower_span_idx = []
 for i in range(len(df)):
     for j in range(len(df)):
@@ -63,29 +64,48 @@ for i in range(len(df)):
 df = df.drop(lower_span_idx)
 
 # Disambiguate 2: disambiguate duplicate tokens
-"TO DO: cycle on duplicate tokens"
+df_dedup = df.groupby('token')['entity_type'].apply(lambda x: list(x)).reset_index()
+for i in range(len(df_dedup)):
+    if len(df_duplicates["entity"][i]) > 1:
+        token_dup = df_dedup["token"][i]
+        ent_dup = df_duplicates["entity"][i]
 
-duplicate = token
-ent_duplicates = []
+        for i,n in enumerate(ent_dup): # replace NER codes with text for prompt
+            if n == 'PERS':
+                ent_dup[i] = 'person'
+            elif n == 'LOC':
+                ent_dup[i] = 'location'
+            elif n == 'ORG':
+                ent_dup[i] = 'organization'
+            elif n == 'TIME':
+                ent_dup[i] = 'date'
+            elif n == 'PROD.MED':
+                ent_dup[i] = 'media'
+            elif n == 'PROD.DOC':
+                ent_dup[i] = 'doctrine'
 
-"TO DO: prompts has to be adapted to the number of options (2, 3, 4, 5 or 6)"
-    
-if len(ent_duplicates) == 2:
-    prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {} or a {}. Give only one answer".format(duplicate, ent_1, ent_2)
-elif len(ent_duplicates) == 3:
-    prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {} or a {}. Give only one answer".format(duplicate, ent_1, ent_2, ent_3)
-elif len(ent_duplicates) == 4:
-    prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {}, a {} or a {}. Give only one answer".format(duplicate, ent_1, ent_2, ent_3, ent_4)
-elif len(ent_duplicates) == 5:
-    prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {}, a {}, a {} or a {}. Give only one answer".format(duplicate, ent_1, ent_2, ent_3, ent_4, ent_5)
-elif len(ent_duplicates) == 6:
-    prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {}, a {}, a {}, a {} or a {}. Give only one answer".format(duplicate, ent_1, ent_2, ent_3, ent_4, ent_5, ent_6)
+        if len(ent_duplicates) == 2:
+            prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {} or a {}? Give only one answer".format(token_dup, ent_dup[0], ent_dup[1])
+        elif len(ent_duplicates) == 3:
+            prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {} or a {}? Give only one answer".format(token_dup, ent_dup[0], ent_dup[1], ent_dup[2])
+        elif len(ent_duplicates) == 4:
+            prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {}, a {} or a {}?. Give only one answer".format(token_dup, ent_dup[0], ent_dup[1], ent_dup[2], ent_dup[3])
+        elif len(ent_duplicates) == 5:
+            prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {}, a {}, a {} or a {}? Give only one answer".format(token_dup, ent_dup[0], ent_dup[1], ent_dup[2], ent_dup[3], ent_dup[4])
+        elif len(ent_duplicates) == 6:
+            prompt = "Input: {}.format(sentence)" + "\n" + "In input, is \"{}\", a {}, a {}, a {}, a {}, a {} or a {}? Give only one answer".format(token_dup, ent_dup[0], ent_dup[1], ent_dup[2], ent_dup[3], ent_dup[4], ent_dup[5])
 
+    answer = T0_infer(sentence)
 
+    # Check that answer is actually an entity type
+    if answer in ent_types:
+        df_duplicates["entity"][i] = [answer]
+    else:
+        df_dedup.drop(i, inplace=True)
 
-    inputs = tokenizer.encode(prompt_YN, return_tensors="pt")
-    inputs = inputs.to("cuda:0")
-    with torch.no_grad():
-        outputs = model.generate(inputs)
+df_dedup["entity"] = df_dedup["entity"].apply(lambda x: x[0])
+df = df_dedup
 
-    answer = (tokenizer.decode(outputs[0], skip_special_tokens=True))
+"TO DO: collapse PROD.MED and PROD.DOC into PROD"
+
+"TO DO: add predicted entity to dataframe with all predictions"
